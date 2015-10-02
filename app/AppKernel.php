437e2338549992aa193fceee9ea338c744560cc4
Justin St. Germain <justin@omedix.com>
2015-10-02 13:26:45 -0700

    integrating JWT... NOT READY

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Salva\PdfJsBundle\SalvaPdfJsBundle(),
+            new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
